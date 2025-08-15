3c7fa41f7bdcf477305fddc85aa68556ab11a4ea
csy969 <96269111+csy969@users.noreply.github.com>
2025-08-15 00:49:17 -0400

    Update index.js

--- index.js
+++ index.js
@@ -58,6 +58,11 @@ function scrollFunction() {
   }
 }
 
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
 
 
 var persona = document.getElementById("persona");
@@ -101,8 +106,3 @@ tobe_btn.onclick = function() {
     tobe.style.display = "block";
 }
 
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-}
